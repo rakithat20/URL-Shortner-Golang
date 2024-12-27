3142706dea390c955deb248147a8b8d940f2bb71
Rakitha <tharupathir@gmail.com>
2024-12-27 09:56:51 +0530

    removed caching

--- internal/Routes/routes.go
+++ internal/Routes/routes.go
@@ -1,13 +1,16 @@
 package routes
 
 import (
+	"net/http"
 	controllers "url-shortner/internal/Controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterRoutes(app *fiber.App) {
+	app.Get("/favicon.ico", func(c *fiber.Ctx) error { return c.Status(http.StatusNoContent).Send(nil) })
 	app.Post("/url", controllers.AddUrlController)
 	app.Get("/:shortURL", controllers.GetLongUrlController)
 	app.Get("/url/:shortURL", controllers.GetUrlInfo)
+
 }
--- main.go
+++ main.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"log"
-	"time"
 
 	routes "url-shortner/internal/Routes"
 	"url-shortner/internal/config"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cache"
 )
 
 func main() {
 	config.ConnectDB()
 	defer config.DB.Close()
 
-	// Initialize a new Fiber app
 	app := fiber.New()
-	app.Use(cache.New(cache.Config{
-		Expiration: 30 * time.Minute, // 30 minutes
-	}))
 
 	routes.RegisterRoutes(app)
-	// Start the server on port 3000
 	log.Fatal(app.Listen(":3000"))
 }
